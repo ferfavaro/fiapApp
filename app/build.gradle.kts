b957ae0fc627d6110a7b11ac87ea02581a38a6d8
KaiqueFj <kaiquelferraz@hotmail.com>
2024-05-07 00:18:47 -0300

    feat: finished the register page (already saving the data into database)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,19 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("com.google.firebase:firebase-auth:22.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -53,5 +60,5 @@ dependencies {
     implementation("com.google.android.material:material:1.1.0")
 
     //Firebase database
-    implementation("com.google.firebase:firebase-database:19.3.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
 }
