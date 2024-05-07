7a47366e56b7931054f3c33d8f843ca20e91ad7d
Fernando Fávaro <fernandofavaro0@gmail.com>
2024-05-07 01:12:52 -0300

    commit: theme adjusts

---
+++ app/src/main/java/com/fiap/fiapapp/ui/home/HomeFragment.kt
@@ -0,0 +1,55 @@
+package com.fiap.fiapapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.SupportMapFragment
+import androidx.lifecycle.ViewModelProvider
+import com.fiap.fiapapp.R
+import com.fiap.fiapapp.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
+
+
+    private lateinit var mMap: GoogleMap
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap!!
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
